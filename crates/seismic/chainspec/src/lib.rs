dcc7f720ae7fc0f5629d684313d450b12eeab738
tx-signer450 <17.parfait_dater@icloud.com>
2025-10-21 18:43:23 -0400

    update enclave bytecode (#254)

--- crates/seismic/chainspec/src/lib.rs
+++ crates/seismic/chainspec/src/lib.rs
@@ -28,7 +28,7 @@ pub const SEISMIC_MAINNET_GENESIS_HASH: B256 =
 /// Calculated by rlp encoding the genesis header and hashing it
 /// Currently matches the mainnet genesis hash because they have matching hardforks
 pub const SEISMIC_DEV_GENESIS_HASH: B256 =
-    b256!("0x655cc1ecc77fe1eab4b1e62a1f461b7fddc9b06109b5ab3e9dc68c144b30c773");
+    b256!("0x89848037d770e47c0fb38ba99efd905cf2e5b4a7e83ff520027b3005d406a86d");
 
 /// Seismic devnet specification
 pub static SEISMIC_DEV: LazyLock<Arc<ChainSpec>> = LazyLock::new(|| {
