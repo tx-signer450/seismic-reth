956f9c407b5cee29f88d33d10cccedce72cf4c6d
tx-signer450 <17.parfait_dater@icloud.com>
2025-11-18 13:37:28 -0500

    Fix timestamp validation (#267)

--- crates/consensus/common/src/validation.rs
+++ crates/consensus/common/src/validation.rs
@@ -320,7 +320,7 @@ pub fn validate_against_parent_timestamp<H: BlockHeader>(
     header: &H,
     parent: &H,
 ) -> Result<(), ConsensusError> {
-    if header.timestamp_seconds() <= parent.timestamp_seconds() {
+    if header.timestamp() <= parent.timestamp() {
         return Err(ConsensusError::TimestampIsInPast {
             parent_timestamp: parent.timestamp_seconds(),
             timestamp: header.timestamp_seconds(),
